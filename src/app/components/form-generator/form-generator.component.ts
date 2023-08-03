8c993ffb611c755da7f77b288bfe170501c2ff32
yasslne <mdyassinechiboub@gmail.com>
2023-08-03 10:17:29 +0100

    fixed background color issue

---
+++ src/app/components/form-generator/form-generator.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-generator',
+  templateUrl: './form-generator.component.html',
+  styleUrls: ['./form-generator.component.css']
+})
+export class FormGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  
+
+}
